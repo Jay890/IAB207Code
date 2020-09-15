8d7001d2bfc4f4ad153ac9f39d988686785cdfd0
Jason Leong <42172421+Jay890@users.noreply.github.com>
2020-09-15 11:11:39 +1000

    Workshop (WIP)
    Add functionatility with fake database for adding comments

--- travel/destinations.py
+++ travel/destinations.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 from travel.models.comment import Comment
 from datetime import datetime
+from travel.models.destination import Destination
 
 # create an instance of the destinations_blueprint
 destinations_blueprint = Blueprint('destination', __name__, url_prefix='/destinations')
@@ -13,8 +14,29 @@ destinations_blueprint = Blueprint('destination', __name__, url_prefix='/destina
 # by setting the id parameter to id, we have to then provide a parameter in the route
 # eg. http://127.0.0.1:5000/destinations/value or http://127.0.0.1:5000/destinations/1
 def show(id):
-    comment_instance = Comment('Bob', 'The food here is pretty good!', datetime.now())
-    return render_template('destinations/show.html', comment=comment_instance)
+    # comment_instance = Comment('Bob', 'The food here is pretty good!', datetime.now())
+    destination_instance = database_get_destination_details()
+    return render_template('destinations/show.html', destination = destination_instance)
+    # return render_template('destinations/show.html', comment=comment_instance)
     # the key value pair of key=id will be found and replaced in destinations/show.html (value in url) to provide some server side rendering
 
- 
+
+# So we need to create an instance of destination
+# Create some comments
+# Create a list of comments
+# We need to pass the comments through the instance of destination 
+
+# dummy database call
+def database_get_destination_details() -> Destination:
+    comment_instance1 = Comment('Bob', 'The food here is pretty good!', datetime.now())
+    comment_instance2 = Comment('John', 'This place is pretty nice!', datetime.now())
+    comment_instance3 = Comment('Dude', 'There are many different restuarants here!', datetime.now())
+
+    mylist = list()
+    mylist.append(comment_instance1)
+    mylist.append(comment_instance2)
+    mylist.append(comment_instance3)
+
+    destination_instance = Destination("Tivoli", "Small town of Rome!!", "https://cdn.thecrazytourist.com/wp-content/uploads/2018/05/ccimage-shutterstock_109711781.jpg","EUR", mylist)
+
+    return destination_instance
--- travel/models/destination.py
+++ travel/models/destination.py
@@ -12,6 +12,12 @@ class Destination:
         self.description = description
         self.image_url = image_url
         self.currency = currency
-        self.comments = comments
+        self.comments = comments 
+        # self.comments = list()
 
+    # version 1
+    # def add_comment(self, comments: Comment):
+    #     self.comments.append(comment)
+ 
+    
     
