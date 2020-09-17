badb3011b606fc4af0d488c62eb7548267c37caa
Jason Leong <42172421+Jay890@users.noreply.github.com>
2020-09-17 12:48:53 +1000

    Workshop 8 (WIP)
    Make some url_for links
    Added buttons for navigation
    Made the UI more friendly

--- travel/views.py
+++ travel/views.py
@@ -17,24 +17,24 @@ def index_controller():
 
 # Now we need to register the blueprint and tell the application that using this Blueprint (__init__.py)
 
-@main_blueprint.route('/new')
-def new_controller():
-    str = "<h2>Well hello there!</h2>"
-    return str
+# @main_blueprint.route('/new')
+# def new_controller():
+#     str = "<h2>Well hello there!</h2>"
+#     return str
 
-@main_blueprint.route('/login', methods=['GET', 'POST'])
-def login_controller():
-    # print(request)
-    # print(request.values.get('email')) #so we can get values grabbing their id. E.g. this is the id set for the email in the login.html
-    # print(request.values.get('pwd'))
-    session['email'] = request.values.get('email')
-    return render_template('login.html')
+# @main_blueprint.route('/login', methods=['GET', 'POST'])
+# def login_controller():
+#     # print(request)
+#     # print(request.values.get('email')) #so we can get values grabbing their id. E.g. this is the id set for the email in the login.html
+#     # print(request.values.get('pwd'))
+#     session['email'] = request.values.get('email')
+#     return render_template('login.html')
 
  
-@main_blueprint.route('/logout')
-def logout_controller():
-    #  session.pop('email', None)
-    session.clear()
-    return "Session is cleared you have been logged out safely."
-# now register the blueprint in __init__.py 
+# @main_blueprint.route('/logout')
+# def logout_controller():
+#     #  session.pop('email', None)
+#     session.clear()
+#     return "Session is cleared you have been logged out safely."
+# # now register the blueprint in __init__.py 
      
